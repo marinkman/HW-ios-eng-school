48cfff8d2547fe89e58d755679dcabe5bb47b6d0
Marina Tihonovich <marinkman@gmail.com>
2022-12-22 12:46:37 +0300

    Add 'return' and use other ways to unwrap optionals

--- HW1_Playground.playground/Contents.swift
+++ HW1_Playground.playground/Contents.swift
@@ -1,110 +1,100 @@
-
-func minNumber(_ number1: Int, _ number2: Int) {
-    if number1 < number2 {
-        number1
-    } else if number1 > number2 {
-        number2
-    } else if number1 == number2 {
-        "Numbers are egual"
-    }
+func minNumber(_ number1: Int, _ number2: Int) -> Int {
+    number1 <= number2 ? number1 : number2
 }
 
-minNumber(0, 0)
+minNumber(1, -3)
 
-func maxNumber(_ number1: Int, _ number2: Int) {
-    if number1 > number2 {
-        number1
-    } else if number1 < number2 {
-        number2
-    } else if number1 == number2 {
-        "Numbers are egual"
-    }
+
+func maxNumber(_ number1: Int, _ number2: Int) -> Int {
+    number1 >= number2 ? number1 : number2
 }
 
 maxNumber(7, -9)
 
 
-func minBetweenThreeNumbers(_ number1: Int, _ number2: Int, _ number3: Int)  {
-    if number1 <= number2 && number1 < number3 {
-        number1
-    } else if number2 <= number1 && number2 < number3 {
-        number2
-    } else if number3 <= number2 && number3 < number1 {
-        number3
-    } else if number1 <= number3 && number1 < number2 {
-        number1
-    } else if number1 == number2 && number1 == number3 {
-        "All numbers are equal"
+func minBetweenThreeNumbers(_ number1: Int, _ number2: Int, _ number3: Int)  -> Int {
+    if number1 <= number2 && number1 <= number3 {
+        return number1
+    } else if number2 <= number1 && number2 <= number3 {
+        return number2
+    } else {
+        return number3
     }
 }
 
-minBetweenThreeNumbers(3, -9, 0)
+minBetweenThreeNumbers(1, -9, 0)
 
 
-func maxBetweenThreeNumbers(_ number1: Int, _ number2: Int, _ number3: Int)  {
-    if number1 >= number2 && number1 > number3 {
-        number1
-    } else if number2 >= number1 && number2 > number3 {
-        number2
-    } else if number3 >= number2 && number3 > number1 {
-        number3
-    } else if number1 >= number3 && number1 > number2 {
-        number1
-    } else if number1 == number2 && number1 == number3 {
-        "All numbers are equal"
+func maxBetweenThreeNumbers(_ number1: Int, _ number2: Int, _ number3: Int)  -> Int {
+    if number1 >= number2 && number1 >= number3 {
+        return number1
+    } else if number2 >= number1 && number2 >= number3 {
+        return number2
+    } else {
+        return number3
     }
 }
 
-maxBetweenThreeNumbers(6, 6, 1)
+maxBetweenThreeNumbers(-6, -6, 1)
+
 
 func returnMinValue(_ array: [Int]) -> Int? {
-    if array.isEmpty { return nil }
     var minValue = array.first
+    
     for value in array {
-        if value < minValue! {
+        if value < minValue ?? value {
             minValue = value
         }
     }
+    
     return minValue
 }
 
 returnMinValue([1, 4, -1, 48, -32])
 
+
 func returnMaxValue(_ array: [Int]) -> Int? {
-    if array.isEmpty { return nil }
     var maxValue = array.first
+    
     for value in array {
-        if value > maxValue! {
+        if value > maxValue ?? value {
             maxValue = value
         }
     }
+    
     return maxValue
 }
 
 returnMaxValue([1, 4, -1, -58, -32])
 
+
 func returnMinValueDouble(_ array: [Double]) -> Double? {
-    if array.isEmpty { return nil }
     var minValueDouble = array.first
+    
     for value in array {
-        if value < minValueDouble! {
+        if value < minValueDouble ?? value {
             minValueDouble = value
         }
     }
+    
     return minValueDouble
 }
 
-returnMinValueDouble([1.2, 0.4, -1.9, 48.3, -0.05])
+returnMinValueDouble([1.2, 0.4, -1.9, 48.3, -0.005])
+
 
 func returnMaxValueDouble(_ array: [Double]) -> Double? {
-    if array.isEmpty { return nil }
     var maxValueDouble = array.first
+    
     for value in array {
-        if value > maxValueDouble! {
+        if value > maxValueDouble ?? value {
             maxValueDouble = value
         }
     }
+    
     return maxValueDouble
 }
 
-returnMaxValueDouble([1.2, 0.4, -1.9, 48.3, -0.05])
+returnMaxValueDouble([1.2, 0.4, -1.9, 48.3, -0.005])
+
+
