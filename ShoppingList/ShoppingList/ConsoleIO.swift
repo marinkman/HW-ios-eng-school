a7e479ed341a486bb441d434956678fc0ed4a071
marinkman <marinkman@gmail.com>
2023-03-17 12:27:53 +0300

    implement the writeLine method

--- ShoppingList/ShoppingList/ConsoleIO.swift
+++ ShoppingList/ShoppingList/ConsoleIO.swift
@@ -3,4 +3,9 @@ struct ConsoleIO {
     func write(_ items: Any...) {
         items.forEach { Swift.print($0, terminator: "") }
     }
+    
+    func writeLine(_ items: Any...) {
+        items.forEach { Swift.print($0, terminator: "") }
+        Swift.print()
+    }
 }
