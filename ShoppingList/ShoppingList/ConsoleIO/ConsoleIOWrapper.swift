2b1a98d3021dd55588c1faf80782a29e32aa16c8
marinkman <marinkman@gmail.com>
2023-03-19 13:03:48 +0300

    remove unnecessary

--- ShoppingList/ShoppingList/ConsoleIO/ConsoleIOWrapper.swift
+++ ShoppingList/ShoppingList/ConsoleIO/ConsoleIOWrapper.swift
@@ -1,26 +1,26 @@
 /// Represents a wrapper for standart input and output streams: stdin, stdout, stderr.
- protocol ConsoleIOWrapper {
-     /**
-      Reads a string from standard input through the end of the current line or until EOF is reached.
+protocol ConsoleIOWrapper {
+    /**
+     Reads a string from standard input through the end of the current line or until EOF is reached.
 
-      - Returns: The string of characters read from standard input.
-      */
-     func readLine() -> String
-     
-     /**
-      Writes the textual representations of the given items into the standard output.
+     - Returns: The string of characters read from standard input.
+     */
+    func readLine() -> String
 
-      - Parameters:
-         - items: Zero or more items to print.
-         - itemsSeparator: A string to print between each item.
-         - itemsTerminator: The string to write after all items have been written.
-      */
-     func write(_ items: Any..., itemsSeparator: String, itemsTerminator: String)
+    /**
+     Writes the textual representations of the given items into the standard output.
 
-     /**
-      Writes the error message into error output.
+     - Parameters:
+        - items: Zero or more items to print.
+        - itemsSeparator: A string to print between each item.
+        - itemsTerminator: The string to write after all items have been written.
+     */
+    func write(_ items: Any..., itemsSeparator: String, itemsTerminator: String)
 
-      - Parameter errorMessage: Error message to write.
-      */
-     func writeErrorLine(_ errorMessage: String)
- }
+    /**
+     Writes the error message into error output.
+
+     - Parameter errorMessage: Error message to write.
+     */
+    func writeErrorLine(_ errorMessage: String)
+}
