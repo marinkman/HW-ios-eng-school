ab551e7fe77b7a93d92815db4da87b52665d9111
marinkman <marinkman@gmail.com>
2023-03-20 15:44:33 +0300

    implement ShoppingList protocol

--- ShoppingList/ShoppingList/ShoppingListLogic/ShoppingList.swift
+++ ShoppingList/ShoppingList/ShoppingListLogic/ShoppingList.swift
@@ -1,8 +1,29 @@
-//
-//  ShoppingList.swift
-//  ShoppingList
-//
-//  Created by Марина Тихонович on 20.03.23.
-//
+/// Represents the base functional of the shopping list.
+protocol ShoppingList {
+    /// Stores groceries to shop
+    var shoppingList: [String] { get }
 
-import Foundation
+    /**
+     Adds the item in the end of the list.
+
+     - Parameter item: Item to add into a `shoppingList`.
+     - Returns: `true` if the `item` was added into a `shoppingList`, otherwise `false`.
+     */
+    func add(_ item: String) -> Bool
+
+    /**
+     Removes the item from the list.
+
+     - Parameter item: Item to remove from a `shoppingList`.
+     - Returns: `true` if the `item` was removed from a `shoppingList`, otherwise `false`.
+     */
+    func remove(_ item: String) -> Bool
+
+    /**
+     Removes the item at specified index from the list.
+
+     - Parameter index: The position of the elemenmt in the `shoppingList` to remove.
+     - Returns: `true` if the item at `index` was removed from a `shoppingList`, otherwise `false`.
+     */
+    func remove(at index: Int) -> Bool
+}
