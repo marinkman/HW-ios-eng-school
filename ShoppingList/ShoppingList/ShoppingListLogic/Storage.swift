7fb47306421ecd62dc60382590211c5c95060980
marinkman <marinkman@gmail.com>
2023-03-20 17:52:44 +0300

    separate interfaces

--- ShoppingList/ShoppingList/ShoppingListLogic/Storage.swift
+++ ShoppingList/ShoppingList/ShoppingListLogic/Storage.swift
@@ -1,6 +1,11 @@
 /// Represents a shopping list.
 class Storage {
+    // MARK: - Internal interface
+
     private(set) var shoppingList = [String]()
+
+    // MARK: - Private interface
+
     private let maxListCapacity = 10
     private var isFull: Bool {
         shoppingList.count >= maxListCapacity
