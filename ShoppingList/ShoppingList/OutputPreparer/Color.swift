959f4d0ec0d7d63469315805b2da2d65f3b37cc6
marinkman <marinkman@gmail.com>
2023-04-06 20:05:51 +0300

    add documentation to `Color` enum

--- ShoppingList/ShoppingList/OutputPreparer/Color.swift
+++ ShoppingList/ShoppingList/OutputPreparer/Color.swift
@@ -1,5 +1,9 @@
+/// Represents the color of the output messages.
 enum Color {
+    /// Default text color.
     static let silence = "\u{001B}[;m"
+    /// Red text color.
     static let red = "\u{001B}[0;31m"
+    /// Green text color.
     static let green = "\u{001B}[32m"
 }
