53623c637e74e3e48227b3cf2efd9d8822ab744f
marinkman <marinkman@gmail.com>
2023-12-20 13:27:39 +0300

    add methods to the `ConsoleIO` struct

--- StatisticsApplication/StatisticsApplication/Console/ConsoleIO.swift
+++ StatisticsApplication/StatisticsApplication/Console/ConsoleIO.swift
@@ -1,4 +1,37 @@
 import Foundation
 
-/// Concole input and output
-struct ConsoleIO {}
+/// Console input and output.
+struct ConsoleIO {
+    /**
+     Writes a message to the console.
+
+     - Parameters:
+        - message: String to output to console.
+        - output: Type of data output to the console. The default is standard output.
+
+     */
+    static func writeMessage(_ message: String, to output: OutputType = .standard) {
+        switch output {
+        case .standard:
+            print("\(message)")
+        case .error:
+            fputs("Error: \(message)\n", stderr)
+        }
+    }
+
+    /**
+     Reads a string and converts it to an array of strings.
+
+     - Complexity: `O(N)`, where `N` is the length of the readable line.
+
+     - Returns: An array of strings.
+
+     */
+    static func readInput() throws -> [String] {
+        guard let inputString = readLine() else { throw ConsoleIOError.failureToReadInput }
+
+        let result = inputString.split(separator: " ").map { String($0) }
+
+        return result
+    }
+}
