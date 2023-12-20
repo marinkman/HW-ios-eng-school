534d177f897c7d5fbc3e2398bb32c5edd2462fb0
marinkman <marinkman@gmail.com>
2023-12-20 13:37:45 +0300

    create `Command` enum

---
+++ StatisticsApplication/StatisticsApplication/Command.swift
@@ -0,0 +1,8 @@
+/// Command available to the user in the application.
+enum Command {
+    static let calculateMedian = "--median"
+    static let calculateMean = "--mean"
+    static let calculateMode = "--mode"
+    static let showHelp = "--help"
+    static let calculateAllStatistics = "allStatistics"
+}
--- StatisticsApplication/StatisticsApplication/Parser/Parser.swift
+++ StatisticsApplication/StatisticsApplication/Parser/Parser.swift
@@ -1,8 +1,9 @@
-struct Parser {
+enum Parser {
     static func parse(_ rawArguments: [String]) throws -> ParsedArgument {
         guard let firstArgument = rawArguments.first else {
-            throw ParserError.missingArguments }
-        
+            throw ParserError.missingArguments
+        }
+
         let possibleNumbers = Array(rawArguments.dropFirst())
 
         switch firstArgument {
@@ -12,32 +13,32 @@ struct Parser {
                 commandName: Command.calculateMedian,
                 parameters: parsedNumbers
             )
-            
+
             return arguments
-            
+
         case Command.calculateMean:
             let parsedNumbers = try convertStringsToInts(possibleNumbers)
             let arguments = ParsedArgument(
                 commandName: Command.calculateMean,
                 parameters: parsedNumbers
             )
-            
+
             return arguments
-            
+
         case Command.calculateMode:
             let parsedNumbers = try convertStringsToInts(possibleNumbers)
             let arguments = ParsedArgument(
                 commandName: Command.calculateMode,
                 parameters: parsedNumbers
             )
-            
+
             return arguments
-            
+
         case Command.showHelp:
             let arguments = ParsedArgument(
                 commandName: Command.showHelp
             )
-            
+
             return arguments
 
         default:
@@ -46,24 +47,22 @@ struct Parser {
                 commandName: Command.calculateAllStatistics,
                 parameters: parsedNumbers
             )
-            
+
             return arguments
         }
-
     }
-    
+
     private static func convertStringsToInts(_ line: [String]) throws -> [Int] {
         guard !line.isEmpty else { throw ParserError.missingArguments }
-        
+
         var numbers = [Int]()
-        
+
         for element in line {
             if let number = Int(element) {
                 numbers.append(number)
             } else { throw ParserError.invalidArguments }
         }
-        
+
         return numbers
     }
 }
-
